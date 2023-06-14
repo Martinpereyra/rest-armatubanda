5b8fc624baf3e8640d5a293ac5a62117fab60408
Martin Pereyra <pereyramartinn@gmail.com>
2023-06-14 16:47:16 -0300

    Initial commit

---
+++ src/main/java/com/restapi/armatubanda/ArmatubandaApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.armatubanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmatubandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmatubandaApplication.class, args);
+	}
+
+}
