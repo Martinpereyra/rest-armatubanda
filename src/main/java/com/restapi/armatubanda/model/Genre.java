508a62c30beff8862e355367a806d7f02a41c73a
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-06-28 23:03:33 -0300

    Added new entity Genre

---
+++ src/main/java/com/restapi/armatubanda/model/Genre.java
@@ -0,0 +1,33 @@
+package com.restapi.armatubanda.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "userSequenceGenerator")
+    @GenericGenerator(
+            name = "userSequenceGenerator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "userSequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    @Column(name = "genre_id")
+    private int id;
+
+    private String name;
+}
