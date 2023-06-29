6ccc1e3185980d94f0a4c29c954465825a17cec7
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-06-28 23:03:50 -0300

    Implemented CRUD for Genre

---
+++ src/main/java/com/restapi/armatubanda/services/GenreService.java
@@ -0,0 +1,45 @@
+package com.restapi.armatubanda.services;
+
+import com.restapi.armatubanda.model.Genre;
+import com.restapi.armatubanda.repository.GenreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GenreService {
+    private final GenreRepository genreRepository;
+
+    public ResponseEntity<Genre> create(String genreName) {
+        Genre genre = Genre.builder().name(genreName).build();
+        return ResponseEntity.ok(genreRepository.save(genre));
+    }
+
+    public List<Genre> getAll() { return genreRepository.findAll(); }
+
+    public ResponseEntity<Genre> update(Genre updatedInstrument) {
+        Optional<Genre> genre = genreRepository.findById(updatedInstrument.getId());
+        if (genre.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Genre existingInstrument = Genre.builder()
+                .id(updatedInstrument.getId())
+                .name(updatedInstrument.getName())
+                .build();
+        Genre savedInstrument = genreRepository.save(existingInstrument);
+        return ResponseEntity.ok(savedInstrument);
+    }
+
+    public String delete(int id) {
+        if (genreRepository.existsById(id)) {
+            genreRepository.deleteById(id);
+            return "Género eliminado correctamente";
+        } else {
+            return "El género con ID " + id + " no existe";
+        }
+    }
+}
