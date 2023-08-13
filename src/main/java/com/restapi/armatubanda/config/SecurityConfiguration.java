755c7ee1f91fbfc4d31322df7667dea5a9934762
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-08-13 20:00:27 -0300

    Cors configuration

--- src/main/java/com/restapi/armatubanda/config/SecurityConfiguration.java
+++ src/main/java/com/restapi/armatubanda/config/SecurityConfiguration.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -22,9 +29,10 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf((csrf) -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((requests)->requests
-                        .requestMatchers("/api/bands/**","/api/musician/**").authenticated()
+                        .requestMatchers("/api/bands/**","/api/musician/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/instrument/**").permitAll()
@@ -33,6 +41,18 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.setAllowedMethods(Arrays.asList( "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
--- src/main/java/com/restapi/armatubanda/controller/MusicianController.java
+++ src/main/java/com/restapi/armatubanda/controller/MusicianController.java
@@ -21,7 +21,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/musician")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class MusicianController {
 
     private final MusicianService musicianService;
